f37510bcccb0ce792f337303c741e3e5c2e41581
JunFe23 <ogum0405@hanmail.net>
2022-08-19 22:45:14 +0900

    가나다 순서 정렬 숙제 진행

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [ 글제목 , 글제목변경 ] = useState(['남자 코트 추천','강남 우동맛집', '파이썬 독학']);
+  let [글제목 , 글제목변경] = useState(['남자 코트 추천','강남 우동맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -17,10 +17,21 @@ function App() {
       </div>
 
       <button onClick={()=>{
+
+        let arr = [1,2,3];
+
         let copy = [...글제목];
         copy[0] = '여자 코트 추천';
         글제목변경(copy);
       }}>글수정</button>
+
+      <button onClick={()=>{
+        
+        let copy2 = [...글제목];
+        copy[0] = '';
+        글순서변경(copy2);
+
+      }}>가나다순정렬</button>
 
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1)}}>👌</span> {따봉} </h4>
